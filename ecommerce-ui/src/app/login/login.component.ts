718d96498f94163d1461f2e9009a64037c10b006
learnerVarsha <learner.varsha@gmail.com>
2025-02-04 08:55:51 -0500

    login component ts file code fixed, css file added

--- ecommerce-ui/src/app/login/login.component.ts
+++ ecommerce-ui/src/app/login/login.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../service/auth/auth.service';
-import { Router } from 'express';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: false,
-  
+
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
+
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   loginForm!: FormGroup;
   hidePassword = true;
@@ -21,31 +22,27 @@ export class LoginComponent {
     private authService: AuthService,
     private router: Router,
     private snackbar: MatSnackBar
-  ){}
+  ) { }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       email: [null, [Validators.required]],
       password: [null, [Validators.required]]
     })
   }
 
-  togglePasswordVisibility(){
+  togglePasswordVisibility() {
     this.hidePassword = !this.hidePassword;
   }
 
-  onSubmit(): void{
-    const username = this.loginForm.get('email')!.value;
-    const password = this.loginForm.get('password')!.value;
+  onSubmit(): void {
+    const username = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
 
-    this.authService.login(username, password).subscribe(
-      (res)=>{
-        this.snackbar.open('Successfully logged in!', 'OK', {duration: 5000});
-      },
-      (error) => {
-        this.snackbar.open('Invalid username and/or password', 'ERROR', {duration: 5000});
-      }
-    );
+    this.authService.login(username, password).subscribe({
+      next: (res) => { this.snackbar.open('Successfully logged in!', 'OK', { duration: 5000 }); },
+      error: (error) => { this.snackbar.open('Invalid username and/or password', 'X', { duration: 5000 }); }
+    });
 
   }
 }
